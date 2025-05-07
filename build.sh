265f4d354051e7d3cbafa5f329b1bda0805a27e0
originpi <2216953145@qq.com>
2025-05-07 18:42:04 +0800

    update

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ catkin_make -DROS_EDITION=ROS1 -DCATKIN_WHITELIST_PACKAGES="livox_ros_driver2"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="fast_lio"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="quadrotor_msgs;cmake_utils;map_generator;plan_env;path_searching;bspline_opt;pose_utils;odom_visualization;local_sensing_node;mockamap;so3_control;multi_map_server;traj_utils;ego_planner;uav_utils;so3_quadrotor_simulator;rviz_plugins;waypoint_generator"
 catkin_make -DCATKIN_WHITELIST_PACKAGES="my_mavros"
+catkin_make -DCATKIN_WHITELIST_PACKAGES="ar_demo;benchmark_publisher;camera_model;data_generator;feature_tracker;pose_graph;vins_estimator"
 # if [ "$1" = "lio" ]; then
 #     echo "build fast_lio"
 #     catkin_make -DCATKIN_WHITELIST_PACKAGES="fast_lio"
